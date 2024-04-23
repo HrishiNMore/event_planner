9012d4d120c6fab7932f6e55e6a65f29f14905ef
HrishiNMore <hrishikeshmore401@gmail.com>
2024-04-23 16:38:39 +0530

    updatet testNG.xml

--- src/test/java/com/ultralesson/eventplanner/EventPlannerTest.java
+++ src/test/java/com/ultralesson/eventplanner/EventPlannerTest.java
@@ -4,7 +4,6 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class EventPlannerTest {
-
     @Test(groups = {"eventCreation"})
     public void testEventCreation() {
         // Implementation of event creation
