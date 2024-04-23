a86b4c9c91b87342235dcddc22a80f142f1b128d
HrishiNMore <hrishikeshmore401@gmail.com>
2024-04-23 17:58:28 +0530

    parameterized venue test-2

--- src/test/java/com/ultralesson/eventplanner/ParameterizedVenueTest.java
+++ src/test/java/com/ultralesson/eventplanner/ParameterizedVenueTest.java
@@ -1,6 +1,9 @@
 package com.ultralesson.eventplanner;
 
+import com.ultralesson.eventplanner.exceptions.VenueException;
+import org.testng.Assert;
 import org.testng.annotations.Factory;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import com.ultralesson.eventplanner.model.Venue;
 
@@ -17,24 +20,32 @@ public class ParameterizedVenueTest {
         this.capacity = capacity;
     }
 
-    @Factory(dataProvider = "venueDataProvider")
-    public static Object[] createInstances(int id, String name, String address, int capacity) {
-        return new Object[] {new ParameterizedVenueTest(id, name, address, capacity)};
-    }
-
-    @Test
-    public void testVenueCreation() {
-        // Test the creation of the Venue instance
-    }
-
-    // Additional test methods as required
-
-    // Data provider supplying test data
-    @org.testng.annotations.DataProvider(name = "venueDataProvider")
+    @DataProvider(name = "venueDataProvider")
     public static Object[][] provideVenueData() {
         return new Object[][] {
                 {1, "Venue A", "Address A", 100},
                 {2, "Venue B", "Address B", 0}, // to result in a VenueException for zero capacity
+                // Add more test data as required
         };
+    }
+
+    @Factory(dataProvider = "venueDataProvider")
+    public static Object[] createInstances(int id, String name, String address, int capacity) {
+        return new Object[] { new ParameterizedVenueTest(id, name, address, capacity) };
+    }
+
+    @Test
+    public void testVenueCreation() {
+        Venue venue = new Venue(id, name, address, capacity);
+        Assert.assertNotNull(venue, "Venue creation failed");
+        Assert.assertEquals(venue.getId(), id, "Venue ID mismatch");
+        Assert.assertEquals(venue.getName(), name, "Venue name mismatch");
+        Assert.assertEquals(venue.getAddress(), address, "Venue address mismatch");
+        Assert.assertEquals(venue.getCapacity(), capacity, "Venue capacity mismatch");
+    }
+
+    @Test(expectedExceptions = VenueException.class)
+    public void testVenueCreationWithInvalidCapacity() {
+        new Venue(id, name, address, capacity);
     }
 }
